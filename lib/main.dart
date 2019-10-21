5461f7903882c7908da3d52c4c09a95d933f5c52
Nguyen Dai <ngtrdai290197@gmail.com>
2019-10-21 16:56:30 +0700

    Init App

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: "This is my first Flutter App", home: new Scaffold());
+  }
+}
