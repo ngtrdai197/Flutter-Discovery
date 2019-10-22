cf3c750b2f72aba38b650c7b88045aa35fbe8607
Nguyen Dai <ngtrdai290197@gmail.com>
2019-10-22 20:34:03 +0700

    (feat): Layout With Grid View Contains Multiple Image

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,44 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  Widget _buildGrid() => GridView.extent(
+      maxCrossAxisExtent: 150,
+      padding: const EdgeInsets.all(4),
+      mainAxisSpacing: 4,
+      crossAxisSpacing: 4,
+      children: _buildGridTileList(30));
+
+  List<Stack> _buildGridTileList(int count) => List.generate(
+      count,
+      (int index) => Stack(
+            alignment: const Alignment(0.0, 0.0),
+            children: <Widget>[
+              Container(
+                child: Image.asset('images/pic${index}.jpg'),
+              ),
+              Container(
+                padding: EdgeInsets.only(
+                    left: 10.0, top: 5.0, right: 10.0, bottom: 5.0),
+                decoration:
+                    BoxDecoration(color: Color.fromARGB(130, 71, 230, 236)),
+                child: Text(
+                  "${index + 1}",
+                  style: TextStyle(color: Colors.white, fontSize: 23.0),
+                ),
+              )
+            ],
+          ));
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
         title: "This is my first Flutter App",
-        home: new Scaffold(
-            appBar: new AppBar(
-              title: new Text("List of English words"),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("List of English words"),
             ),
-            body: new Center(
-              child: new Text("Hello, world"),
+            body: Center(
+              child: _buildGrid(),
             )));
   }
 }
