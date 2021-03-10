68b3c2114c23b0ebe8beaadd940e6f037eb77439
Dai Nguyen <ngtrdai290197@gmail.com>
2021-03-10 17:04:20 +0700

    fix(): Fix some errors and build with macos

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,20 @@ import 'package:myapp/screens/home_screen.dart';
 import 'package:myapp/screens/login_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'screens/home_screen.dart';
 import 'services/http_interceptor.dart';
 
 void main() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   final String token = prefs.getString('token');
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp(token: token));
 }
 
 class MyApp extends StatelessWidget {
   final String token;
   MyApp({Key key, @required this.token}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
